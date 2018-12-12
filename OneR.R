f30dea38b65f774e2e9d14ebeac670124a7aa1ea
William <w255li@edu.uwaterloo.ca>
2018-12-12 18:15:38 -0500

    Added Predictions to OneR

--- OneR.R
+++ OneR.R
@@ -3,5 +3,14 @@ training_index <- createDataPartition(data$Compliant..Y.N., p = 1/3, list = FALS
 data_training <- data[training_index, ]
 data_testing <- data[-training_index, ]
 
+#train model
 model <- OneR(data_training[,c(12,1:11)], verbose = TRUE)
 
+#make predictions
+predictions <- predict(model, data_testing)
+
+predictions
+
+
+#confusion Matrix of Predictions
+confusionMatrix(predictions, data_testing$Compliant..Y.N.)
