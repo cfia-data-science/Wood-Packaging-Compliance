13265962e3d7d92c5b82fcebf0874d4ab92f4e0d
William <w255li@edu.uwaterloo.ca>
2018-12-12 18:26:48 -0500

    Update OneR.R

--- OneR.R
+++ OneR.R
@@ -1,16 +1,14 @@
 #data partition
-training_index <- createDataPartition(data$Compliant..Y.N., p = 1/3, list = FALSE)
+training_index <- createDataPartition(data$Compliant..Y.N., p = 0.8, list = FALSE)
 data_training <- data[training_index, ]
 data_testing <- data[-training_index, ]
 
 #train model
-model <- OneR(data_training[,c(12,1:11)], verbose = TRUE)
+model <- OneR(data_training[,c(1:10,12)], verbose = TRUE)
 
 #make predictions
 predictions <- predict(model, data_testing)
 
-predictions
-
 
 #confusion Matrix of Predictions
-confusionMatrix(predictions, data_testing$Compliant..Y.N.)
+table(predictions,data_testing$Compliant..Y.N.)
