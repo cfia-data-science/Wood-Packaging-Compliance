817a274e3338c14b2ee559d5d95e70bf50cdf363
William <w255li@edu.uwaterloo.ca>
2018-12-13 11:02:53 -0500

    Added comments

--- NaiveBayes.R
+++ NaiveBayes.R
@@ -3,13 +3,16 @@ library(e1071)
 library(naivebayes)
 set.seed(7267166)
 
+#Create model
 NBclassfier <- naiveBayes(Compliant..Y.N. ~ Shipper.Country + Port.of.Entry..map. + Goods.Category + Month + Packaging.Material , 
                          data=cart_data_training)
-print(NBclassfier)
 
+#make predictions on testing set
 testPred <- predict(NBclassfier, newdata= cart_data_testing, type = "class")
 
+#confusion matrix
 confusionMatrix(testPred, cart_data_testing$Compliant..Y.N.)
+
 
 df_predicting <- data.frame(
   Packaging.Material = "MP",
