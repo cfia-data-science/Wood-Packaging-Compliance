c5c09029866c6f6fa89f1f041ffa348186c2fe70
William <w255li@edu.uwaterloo.ca>
2018-12-14 14:41:28 -0500

    Update NaiveBayes.R

--- NaiveBayes.R
+++ NaiveBayes.R
@@ -1,11 +1,19 @@
 library(caret)
 library(e1071)
+library(ROSE)
 library(naivebayes)
 set.seed(7267166)
 
+#data partition
+training_index <- createDataPartition(data$Compliant..Y.N., p = 0.8, list = FALSE)
+data_training <- data[training_index, ]
+data_testing <- data[-training_index, ]
+
+data_under<-ovun.sample(Compliant..Y.N.~., data=data_training, p=0.5, seed=1,  method="under")
+
 #Create model
-NBclassfier <- naiveBayes(Compliant..Y.N. ~ Shipper.Country + Port.of.Entry..map. + Goods.Category + Month + Packaging.Material , 
-                         data=cart_data_training)
+NBclassfier <- naiveBayes(Compliant..Y.N. ~ Shipper.Country + Port.of.Entry..map. + Goods.Category + Packaging.Material , 
+                         data=data_training)
 
 #make predictions on testing set
 testPred <- predict(NBclassfier, newdata= cart_data_testing, type = "class")
