adcfc81789a8f10eea26ae205377454936a61a9d
IsaacGuan <yanranguan@gmail.com>
2019-02-27 09:44:00 -0500

    update SVM models

--- SVM.R
+++ SVM.R
@@ -1,41 +1,89 @@
-#data partitions
-training_index <- createDataPartition(data$Compliant..Y.N., p = 0.8, list = FALSE)
-data_training <- data[training_index, ]
-data_testing <- data[-training_index, ]
-#undersampled dataset
-data_under<-ovun.sample(Compliant..Y.N.~., data=data_training, p=0.5, seed=1,  method="under")$data
-#oversampled dataset
-data_over<-ovun.sample(Compliant..Y.N.~., data=data_training,  p=0.5, seed=1,  method="over")$data
+# linear svm
+set.seed(1)
+svmLinear_original <- svm(Compliant..Y.N. ~ Shipper.Country + Port.of.Entry..map. + Goods.Category + Packaging.Material,
+                          kernel = "linear",
+                          data = data_training,
+                          cross = 10)
 
-#SVM model without data balancing
-svm_model <- svm(Compliant..Y.N. ~ Shipper.Country + Port.of.Entry..map. + Goods.Category + Packaging.Material , 
-                                       data=data)
-pred_SVM <- predict(svm_model, newdata= data_testing, type = "class")
+# original model predictions
+predictions_svmLinear_original <- predict(svmLinear_original, data_testing)
+cm_svmLinear_original <- confusionMatrix(predictions_svmLinear_original, data_testing$Compliant..Y.N.)
+cm_svmLinear_original
+cm_svmLinear_original$byClass["F1"]
+gmean_svmLinear_original <- unname((cm_svmLinear_original$byClass["Specificity"] * cm_svmLinear_original$byClass["Sensitivity"]) ^ 0.5)
+gmean_svmLinear_original
 
-confusionMatrix(pred_SVM, data_testing$Compliant..Y.N.)
+# linear svm using under-sampled data
+set.seed(3)
+svmLinear_under <- svm(Compliant..Y.N. ~ Shipper.Country + Port.of.Entry..map. + Goods.Category + Packaging.Material,
+                       kernel = "linear",
+                       data = data_under,
+                       cross = 10)
 
-#SVM model with undersampling
-svm_model_under <- svm(Compliant..Y.N. ~ Shipper.Country + Port.of.Entry..map. + Goods.Category + Packaging.Material , 
-                 data=data_under)
-pred_SVM_under <- predict(svm_model_under, newdata= data_testing, type = "class")
+# under-sampled model predictions
+predictions_svmLinear_under <- predict(svmLinear_under, data_testing)
+cm_svmLinear_under <- confusionMatrix(predictions_svmLinear_under, data_testing$Compliant..Y.N.)
+cm_svmLinear_under
+cm_svmLinear_under$byClass["F1"]
+gmean_svmLinear_under <- unname((cm_svmLinear_under$byClass["Specificity"] * cm_svmLinear_under$byClass["Sensitivity"]) ^ 0.5)
+gmean_svmLinear_under
 
-confusionMatrix(pred_SVM, data_testing$Compliant..Y.N.)
+# linear svm using rose data
+set.seed(5)
+svmLinear_rose <- svm(Compliant..Y.N. ~ Shipper.Country + Port.of.Entry..map. + Goods.Category + Packaging.Material,
+                      kernel = "linear",
+                      data = data_rose,
+                      cross = 10)
 
+# rose model predictions
+predictions_svmLinear_rose <- predict(svmLinear_rose, data_testing)
+cm_svmLinear_rose <- confusionMatrix(predictions_svmLinear_rose, data_testing$Compliant..Y.N.)
+cm_svmLinear_rose
+cm_svmLinear_rose$byClass["F1"]
+gmean_svmLinear_rose <- unname((cm_svmLinear_rose$byClass["Specificity"] * cm_svmLinear_rose$byClass["Sensitivity"]) ^ 0.5)
+gmean_svmLinear_rose
 
-#SVM model with oversampling
-svm_model_over <- svm(Compliant..Y.N. ~ Shipper.Country + Port.of.Entry..map. + Goods.Category + Packaging.Material , 
-                       data=data_over)
-pred_SVM_over <- predict(svm_model_over, newdata= data_testing, type = "class")
+# radial based svm
+set.seed(1)
+svmRadial_original <- svm(Compliant..Y.N. ~ Shipper.Country + Port.of.Entry..map. + Goods.Category + Packaging.Material, 
+                          kernel = "radial",
+                          data = data_training,
+                          cross = 10)
 
-confusionMatrix(pred_SVM, data_testing$Compliant..Y.N.)
+# original model predictions
+predictions_svmRadial_original <- predict(svmRadial_original, data_testing)
+cm_svmRadial_original <- confusionMatrix(predictions_svmRadial_original, data_testing$Compliant..Y.N.)
+cm_svmRadial_original
+cm_svmRadial_original$byClass["F1"]
+gmean_svmRadial_original <- unname((cm_svmRadial_original$byClass["Specificity"] * cm_svmRadial_original$byClass["Sensitivity"]) ^ 0.5)
+gmean_svmRadial_original
 
-#SVM alternate implementation using CARET
-SVM <- train (y = data_training$Compliant..Y.N., 
-              x = data_training[,c(3,8,9,10)], 
-              method = "svmPoly", 
-              allowParallel = TRUE,
-              trControl = trainControl(method = "cv", number = 10))
+# radial based svm using under-sampled data
+set.seed(3)
+svmRadial_under <- svm(Compliant..Y.N. ~ Shipper.Country + Port.of.Entry..map. + Goods.Category + Packaging.Material, 
+                       kernel = "radial",
+                       data = data_under,
+                       cross = 10)
 
-pred <- predict(SVM, newdata= data_testing, type = "class")
+# under-sampled model predictions
+predictions_svmRadial_under <- predict(svmRadial_under, data_testing)
+cm_svmRadial_under <- confusionMatrix(predictions_svmRadial_under, data_testing$Compliant..Y.N.)
+cm_svmRadial_under
+cm_svmRadial_under$byClass["F1"]
+gmean_svmRadial_under <- unname((cm_svmRadial_under$byClass["Specificity"] * cm_svmRadial_under$byClass["Sensitivity"]) ^ 0.5)
+gmean_svmRadial_under
 
-confusionMatrix(pred, data_testing$Compliant..Y.N.)
+# radial based svm using rose data
+set.seed(5)
+svmRadial_rose <- svm(Compliant..Y.N. ~ Shipper.Country + Port.of.Entry..map. + Goods.Category + Packaging.Material, 
+                      kernel = "radial",
+                      data = data_rose,
+                      cross = 10)
+
+# rose model predictions
+predictions_svmRadial_rose <- predict(svmRadial_rose, data_testing)
+cm_svmRadial_rose <- confusionMatrix(predictions_svmRadial_rose, data_testing$Compliant..Y.N.)
+cm_svmRadial_rose
+cm_svmRadial_rose$byClass["F1"]
+gmean_svmRadial_rose <- unname((cm_svmRadial_rose$byClass["Specificity"] * cm_svmRadial_rose$byClass["Sensitivity"]) ^ 0.5)
+gmean_svmRadial_rose
