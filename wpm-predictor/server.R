b9c3a1f187b7d3e3c8aac306a403f45fdad4466d
IsaacGuan <yanranguan@gmail.com>
2019-03-20 13:32:41 -0400

    update UI for wpm-predictor

--- wpm-predictor/server.R
+++ wpm-predictor/server.R
@@ -1,89 +1,83 @@
 library(shiny)
+library(caret)
 
-load("rpartData.Rdata")
+load("model.Rdata")
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-  print("foo")
-  input_shipper_country <- reactive({
-    if (input$shipper_country %in% list_africa_other) {
-      "Africa Other"
-    } else if (input$shipper_country %in% list_americas_other) {
-      "Americas Other"
-    } else if (input$shipper_country %in% list_asia_other) {
-      "Asia Other"
-    } else if (input$shipper_country %in% list_europe_other) {
-      "Europe Other"
-    } else if (input$shipper_country %in% list_oceania_other) {
-      "Oceania Other"
-    } else{
-      input$shipper_country
-    }
-  })
-  df_predicting <- reactive({
-    if (input$packaging_material != "Unknown") {
+shinyServer(
+  function(input, output) {
+    df_predicting <- reactive({
       df_predicting <- data.frame(
         Packaging.Material = input$packaging_material,
         Port.of.Entry..map. = input$port_of_entry,
-        Shipper.Country = input_shipper_country(),
-        Goods.Category = input$goods_category,
-        Month = input$month
+        Shipper.Country = input$shipper_country,
+        Goods.Category = input$goods_category
       )
-    }
-    else{
-      data.frame(
-        Port.of.Entry..map. = input$port_of_entry,
-        Shipper.Country = input_shipper_country(),
-        Goods.Category = input$goods_category,
-        Month = input$month
-      )
-    }
+    })
     
-  })
-  
-  model <- reactive({
-    if (input$packaging_material != "Unknown") {
-      rtree.fit
-    } else {
-      rtree.fit2
-    }
-  })
-  
-  output$result_raw <- renderText({
-    result_raw <- predict(model(), df_predicting(), type = "class")
-    if (result_raw == "Y") {
-      paste(
-        "The package is predicted as ",
-        "<font color=\"#008000\"><b>",
-        "Compliant",
-        "</b></font>"
-      )
-    } else {
-      paste(
-        "The package is predicted as ",
-        "<font color=\"#FF0000\"><b>",
-        "Non-compliant",
-        "</b></font>"
-      )
-    }
-  })
-  output$result_prob <- renderText({
-    result_prob <- predict(model(), df_predicting(), type = "prob")
-    if (result_prob[2] < 0.5) {
-      paste(
-        "The likelihood of non-compliance is ",
-        "<font color=\"#008000\"><b>",
-        percent(as.numeric(result_prob[2])),
-        "</b></font>"
-      )
-    } else {
-      paste(
-        "The likelihood of non-compliance is ",
-        "<font color=\"#FF0000\"><b>",
-        percent(as.numeric(result_prob[2])),
-        "</b></font>"
-      )
-    }
-    
-  })
-})
+    output$result <- renderText({
+      result_prob <- predict(cart_down, df_predicting(), type = "prob")
+      if (result_prob[2] < 0.5) {
+        paste(
+          "<div class='result-row'>",
+          "<img src='compliant.png' class='circle-img' class='result-img' width='150' height='150'></img>",
+          "<svg class='circle-chart' viewbox='0 0 33.83098862 33.83098862' width='150' height='150' xmlns='http://www.w3.org/2000/svg'>",
+          "<circle class='circle-chart__background' stroke='#3fa9f5' stroke-width='2' fill='none' cx='16.91549431' cy='16.91549431' r='15.91549431'/>",
+          "<circle class='circle-chart__circle' stroke='#f15a24' stroke-width='2' stroke-dasharray='",
+          as.numeric(result_prob[2])*100,
+          ",100' stroke-linecap='round' fill='none' cx='16.91549431' cy='16.91549431' r='15.91549431'/>",
+          "<g class='circle-chart__info'>",
+          "<text class='circle-chart__percent' x='16.91549431' y='17' alignment-baseline='central' text-anchor='middle' fill='#3fa9f5' font-size='8'>",
+          percent(as.numeric(1 - result_prob[2])),
+          "</text>",
+          "</text></g></svg></div>",
+          "<div class='result-row'>",
+          "<div class='result-column1'><b>Predicted as</b></div><div class='result-column2'><b>Compliance:</b></div>",
+          "</div>",
+          "<div class='result-row'>",
+          "<div class='result-column1'><font color='#56AB2F'><b>Compliant</b></font></div><div class='result-column2'><font color='#56AB2F'><b>",
+          percent(as.numeric(1 - result_prob[2])),
+          "</b></font></div></div>"
+        )
+        # paste("<div>",
+        #       "The package is predicted as ",
+        #       "<font color=\"#008000\"><b>",
+        #       "Compliant",
+        #       "</b></font></div>",
+        #       "<div>The likelihood of non-compliance is ",
+        #       "<font color=\"#008000\"><b>",
+        #       percent(as.numeric(result_prob[2])),
+        #       "</b></font></div>")
+      } else {
+        paste(
+          "<div class='result-row'>",
+          "<img src='error.png' class='circle-img' class='result-img' width='150' height='150'></img>",
+          "<svg class='circle-chart' viewbox='0 0 33.83098862 33.83098862' width='150' height='150' xmlns='http://www.w3.org/2000/svg'>",
+          "<circle class='circle-chart__background' stroke='#3fa9f5' stroke-width='2' fill='none' cx='16.91549431' cy='16.91549431' r='15.91549431'/>",
+          "<circle class='circle-chart__circle' stroke='#f15a24' stroke-width='2' stroke-dasharray='",
+          as.numeric(result_prob[2])*100,
+          ",100' stroke-linecap='round' fill='none' cx='16.91549431' cy='16.91549431' r='15.91549431'/>",
+          "<g class='circle-chart__info'>",
+          "<text class='circle-chart__percent' x='16.91549431' y='17' alignment-baseline='central' text-anchor='middle' fill='#f15a24' font-size='8'>",
+          percent(as.numeric(result_prob[2])),
+          "</text>",
+          "</text></g></svg></div>",
+          "<div class='result-row'>",
+          "<div class='result-column1'><b>Predicted as</b></div><div class='result-column2'><b>Non-compliance:</b></div>",
+          "</div>",
+          "<div class='result-row'>",
+          "<div class='result-column1'><font color='#FF4B2B'><b>Non-compliant</b></font></div><div class='result-column2'><font color='#FF4B2B'><b>",
+          percent(as.numeric(result_prob[2])),
+          "</b></font></div></div>"
+        )
+        # paste("<div>The package is predicted as ",
+        #       "<font color=\"#FF0000\"><b>",
+        #       "Non-compliant",
+        #       "</b></font></div>",
+        #       "<div>The likelihood of non-compliance is ",
+        #       "<font color=\"#FF0000\"><b>",
+        #       percent(as.numeric(result_prob[2])),
+        #       "</b></font></div>")
+      }
+    })
+  }
+)
--- wpm-predictor/ui.R
+++ wpm-predictor/ui.R
@@ -1,36 +1,87 @@
 library(shiny)
+library(shinyjs)
 
-load("rpartData.Rdata")
+load("model.Rdata")
 
 shinyUI(
   fluidPage(
-    titlePanel(title = "WPM Compliance Calculator"),
+    theme = "custom.css",
+    useShinyjs(),
     fluidRow(
-      sidebarPanel(
-        radioButtons("packaging_material",
-                     "Packaging Material",
-                     c("WPM" = "WPM",
-                       "WPM & MP" = "WPM & MP",
-                       "MP" = "MP",
-                       "Unknown" = "Unknown"),
-                     inline = FALSE),
-        selectInput("port_of_entry",
-                    "Port of Entry",
-                    list_port_of_entry),
-        selectInput("shipper_country",
-                    "Shipper Country",
-                    list_shipper_country),
-        selectInput("goods_category",
-                    "Goods Category",
-                    list_goods_category),
-        selectInput("month",
-                    "Month",
-                    list_month)
+      class = "main-wrapper",
+      column(
+        6,
+        class = "section left arrow-right",
+        div(
+          class = "container",
+          div(
+            class = "questions-wrapper",
+            h2(
+              "WPM Compliance Calculator"
+            ),
+            HTML('<div id="packaging_material" class="form-group shiny-input-radiogroup shiny-input-container shinyjs-resettable shiny-bound-input" data-shinyjs-resettable-id="packaging_material" data-shinyjs-resettable-type="RadioButtons" data-shinyjs-resettable-value="WPM">'),
+            tags$label(
+              "Packaging Material",
+              class = "control-label"
+            ),
+            div(
+              class = "shiny-options-group radio-group radio-group-wrapper",
+              tags$ul(
+                tags$li(
+                  tags$input(type = "radio",
+                             name = "packaging_material",
+                             id = "WPM",
+                             value = "WPM",
+                             checked = "checked"),
+                  HTML('<div class="check"></div><label for="WPM">WPM</label>')
+                ),
+                tags$li(
+                  tags$input(type = "radio",
+                             name = "packaging_material",
+                             id = "WPM & MP",
+                             value = "WPM & MP"),
+                  HTML('<div class="check"><div class="inside"></div></div><label for="WPM & MP">WPM & MP</label>')
+                ),
+                tags$li(
+                  tags$input(type = "radio",
+                             name = "packaging_material",
+                             id = "MP",
+                             value = "MP"),
+                  HTML('<div class="check"><div class="inside"></div></div><label for="MP">MP</label>')
+                )
+              )
+            ),
+            HTML('</div>'),
+            div(
+              selectInput("port_of_entry",
+                          "Port of Entry",
+                          list_port_of_entry),
+              selectInput("shipper_country",
+                          "Shipper Country",
+                          list_shipper_country),
+              selectInput("goods_category",
+                          "Goods Category",
+                          list_goods_category))
+          )
+        )
       ),
-      mainPanel(
-        h2("Result:"),
-        htmlOutput("result_raw"),
-        htmlOutput("result_prob")
+      column(
+        6,
+        class = "section right",
+        div(
+          class = "container",
+          div(
+            class = "result-wrapper",
+            div(
+              class = "result-wrapper-title",
+              h2("Result:")
+            ),
+            div(
+              class = "result-wrapper-content",
+              htmlOutput("result", class = "result")
+            )
+          )
+        )
       )
     )
   )
